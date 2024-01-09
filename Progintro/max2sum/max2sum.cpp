8c3eae123143e34b706d27ce9892cbc8ba01afcc
Sergio Sakellaropoulos <126912076+sergio-sak@users.noreply.github.com>
2024-01-09 16:08:52 +0200

    dsa

---
+++ Progintro/max2sum/max2sum.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+typedef long long ll;
+typedef std::vector<int> vi;
+typedef std::vector<long long> vll;
+typedef std::pair<int, int> pi;
+typedef std::pair<ll, ll> pl;
+
+ll N;
+
+int main()
+{
+    #ifdef CONTEST
+        freopen("file.in", "r", stdin);
+        freopen("file.out", "w", stdout);
+    #else
+        freopen("file.in", "r", stdin);
+    #endif
+    
+    return 0;
+}
