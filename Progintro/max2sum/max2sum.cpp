ab13ffeb5eb0da746366b21c96f23848a8478c6d
Sergio Sakellaropoulos <126912076+sergio-sak@users.noreply.github.com>
2024-01-09 14:52:49 +0000

    max2sum done

--- Progintro/max2sum/max2sum.cpp
+++ Progintro/max2sum/max2sum.cpp
@@ -6,16 +6,24 @@ typedef std::vector<long long> vll;
 typedef std::pair<int, int> pi;
 typedef std::pair<ll, ll> pl;
 
-ll N;
+ll N, temp, max1 = 0, max2 = 0;
+vll max2sum;
 
 int main()
 {
     #ifdef CONTEST
-        freopen("file.in", "r", stdin);
-        freopen("file.out", "w", stdout);
+        freopen("max2sum.in", "r", stdin);
+        freopen("max2sum.out", "w", stdout);
     #else
-        freopen("file.in", "r", stdin);
+        freopen("max2sum.in", "r", stdin);
     #endif
     
+    scanf("%lld", &N);
+    for (int i = 0; i < N; i++) {
+        scanf("%lld", &temp);
+        max1 = std::max(max1, temp+i);
+        max2 = std::max(max2, temp-i);  
+    }
+    printf("%lld\n", max1+max2);
     return 0;
 }
