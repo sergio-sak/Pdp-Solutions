bef221f2492c3262995c2d2958cf96cbbe3ac87d
Sergio Sakellaropoulos <126912076+sergio-sak@users.noreply.github.com>
2024-01-11 10:27:52 +0000

    max2sum fix

--- Progintro/max2sum/max2sum.cpp
+++ Progintro/max2sum/max2sum.cpp
@@ -6,7 +6,7 @@ typedef std::vector<long long> vll;
 typedef std::pair<int, int> pi;
 typedef std::pair<ll, ll> pl;
 
-ll N, temp, max1 = 0, max2 = 0;
+ll N, temp, max1 = -1, max2 = -1, indx;
 vll max2sum;
 
 int main()
@@ -19,10 +19,22 @@ int main()
     #endif
     
     scanf("%lld", &N);
-    for (int i = 0; i < N; i++) {
+
+    for(int i = 0; i < N; i++){
         scanf("%lld", &temp);
-        max1 = std::max(max1, temp+i);
-        max2 = std::max(max2, temp-i);  
+        max2sum.push_back(temp);
+    }
+
+    for (int i = 0; i < N; i++) {
+        if(max1<max2sum[i]+i){
+            max1 = max2sum[i]+i;
+            indx = i;
+        }
+    }
+
+    for(int i = 0; i < N; i++){
+        if(i!=indx && max2<max2sum[i]-i)
+            max2 = max2sum[i]-i;
     }
     printf("%lld\n", max1+max2);
     return 0;
