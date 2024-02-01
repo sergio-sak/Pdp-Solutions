a6e3433547b6e61b80edd0c97db18beaa9df6004
sergio-sak <sakellagi@gmail.com>
2024-02-01 20:48:35 +0200

    Done?

--- Pdp 30/Camp/Pistes/pistes.cpp
+++ Pdp 30/Camp/Pistes/pistes.cpp
@@ -8,13 +8,33 @@ typedef std::pair<ll, ll> pl;
 
 ll N, K, R, S, start_S, temp, max_stars = 0, cur_stars = 0;
 vll cur, cur_perm, chosen;
-std::vector<vll> rest, perms;
+std::vector<vll> rest;
 std::unordered_map<ll, ll> keys;
-bool end = false;
 
 void search(){
     if(cur_perm.size() == N){
-        perms.push_back(cur_perm);
+        std::unordered_map<ll, ll> keys_copy = keys;
+        cur_stars = start_S;
+        for(auto indx : cur_perm){
+            K = rest[indx][0];
+            R = rest[indx][1];
+            S = rest[indx][2];
+            for(int i = 3; i < 3+K; ++i){
+                keys_copy[rest[indx][i]]--;
+                if(keys_copy[rest[indx][i]] < 0){
+                    max_stars = std::max(max_stars, cur_stars);
+                    cur_stars = start_S;
+                    return;
+                }
+            }
+            for(int i = 3+K; i < 3+K+R; ++i){
+                keys_copy[rest[indx][i]]++;
+            }
+            cur_stars += S;
+        }
+            keys = keys_copy;
+            max_stars = std::max(max_stars, cur_stars);
+        //cur_stars = start_S;
     }
     else {
         for (int i = 1; i <= N; ++i){
@@ -23,7 +43,7 @@ void search(){
             cur_perm.push_back(i);
             search();
             chosen[i] = false;
-            cur_perm.pop_back(); 
+            cur_perm.pop_back();
         }
     }
 }
@@ -34,9 +54,9 @@ int main()
         freopen("pistes.in", "r", stdin);
         freopen("pistes.out", "w", stdout);
     #else
-        freopen("pistes.in1", "r", stdin);
+        freopen("Testcases/pistes.in13", "r", stdin);
     #endif
-    
+   
     scanf("%lld", &N);
     rest.push_back({});
     chosen.resize(N+1);
@@ -46,8 +66,11 @@ int main()
         scanf("%lld", &temp);
         keys[temp]++;
     }
+   
+    //std::cout<<K<<" "<<R<<" "<<S<<" "<<temp<<"\n";
     cur_stars = S;
     start_S = S;
+   
     for(int i = 1; i <= N; ++i){
         scanf("%lld %lld %lld", &K, &R, &S);
         cur.push_back(K);
@@ -65,35 +88,6 @@ int main()
         cur.clear();
     }
     search();
-    
-    for(auto perm : perms){
-        std::unordered_map<ll, ll> keys_copy = keys;
-        for(auto indx : perm){
-            K = rest[indx][0];
-            R = rest[indx][1];
-            S = rest[indx][2];
-            for(int i = 3; i < 3+K; ++i){
-                keys_copy[rest[indx][i]]--;
-                if(keys_copy[rest[indx][i]] < 0){
-                    cur_stars = start_S;
-                    end = true;
-                    break;
-                }
-            }
-            if(end){
-                end = false;
-                break;
-            }
-            for(int i = 3+K; i < 3+K+R; ++i){
-                keys_copy[rest[indx][i]]++;
-            }
-            cur_stars += S;
-            keys=keys_copy;
-        }
-        max_stars = std::max(max_stars, cur_stars);
-        cur_stars = start_S;
-    }
-    
     printf("%lld\n", max_stars);
     return 0;
 }
