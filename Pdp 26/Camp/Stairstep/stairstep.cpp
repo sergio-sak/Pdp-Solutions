7e35f8f1c95b48cff9107b244ce23eb5f68024a5
sergio-sak <sakellagi@gmail.com>
2024-02-04 20:32:44 +0200

    Pistes Done! Starting Stairstep

---
+++ Pdp 26/Camp/Stairstep/stairstep.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+typedef long long ll;
+typedef std::vector<int> vi;
+typedef std::vector<long long> vll;
+typedef std::pair<int, int> pi;
+typedef std::pair<ll, ll> pl;
+
+int main()
+{
+    #ifdef CONTEST
+        freopen("stairstep.in", "r", stdin);
+        freopen("stairstep.out", "w", stdout);
+    #else
+        freopen("stairstep.in1", "r", stdin);
+    #endif
+    
+    return 0;
+}
--- Pdp 30/Camp/Pistes/pistes.cpp
+++ Pdp 30/Camp/Pistes/pistes.cpp
@@ -6,47 +6,12 @@ typedef std::vector<long long> vll;
 typedef std::pair<int, int> pi;
 typedef std::pair<ll, ll> pl;
 
-ll N, K, R, S, start_S, temp, max_stars = 0, cur_stars = 0;
-vll cur, cur_perm, chosen;
-std::vector<vll> rest;
-std::unordered_map<ll, ll> keys;
-
-void search(){
-    if(cur_perm.size() == N){
-        std::unordered_map<ll, ll> keys_copy = keys;
-        cur_stars = start_S;
-        for(auto indx : cur_perm){
-            K = rest[indx][0];
-            R = rest[indx][1];
-            S = rest[indx][2];
-            for(int i = 3; i < 3+K; ++i){
-                keys_copy[rest[indx][i]]--;
-                if(keys_copy[rest[indx][i]] < 0){
-                    max_stars = std::max(max_stars, cur_stars);
-                    cur_stars = start_S;
-                    return;
-                }
-            }
-            for(int i = 3+K; i < 3+K+R; ++i){
-                keys_copy[rest[indx][i]]++;
-            }
-            cur_stars += S;
-        }
-            keys = keys_copy;
-            max_stars = std::max(max_stars, cur_stars);
-        //cur_stars = start_S;
-    }
-    else {
-        for (int i = 1; i <= N; ++i){
-            if(chosen[i]) continue;
-            chosen[i] = true;
-            cur_perm.push_back(i);
-            search();
-            chosen[i] = false;
-            cur_perm.pop_back();
-        }
-    }
-}
+int N, K, R, S, start_S, temp;
+int max_stars = 0, cur_stars = 0;
+std::vector<int> perm, cur;
+std::vector<std::vector<int>> rest;
+std::map<int, int> keys;
+bool flag = true;
 
 int main()
 {
@@ -54,40 +19,66 @@ int main()
         freopen("pistes.in", "r", stdin);
         freopen("pistes.out", "w", stdout);
     #else
-        freopen("Testcases/pistes.in13", "r", stdin);
+        freopen("Testcases/pistes.in8", "r", stdin);
     #endif
    
-    scanf("%lld", &N);
+    scanf("%d", &N);
     rest.push_back({});
-    chosen.resize(N+1);
-
-    scanf("%lld %lld %lld", &K, &R, &S);
+    scanf("%d %d %d", &K, &R, &S);
     for(int i = 0; i < R; ++i){
-        scanf("%lld", &temp);
+        scanf("%d", &temp);
         keys[temp]++;
     }
    
-    //std::cout<<K<<" "<<R<<" "<<S<<" "<<temp<<"\n";
     cur_stars = S;
     start_S = S;
    
     for(int i = 1; i <= N; ++i){
-        scanf("%lld %lld %lld", &K, &R, &S);
+        scanf("%d %d %d", &K, &R, &S);
         cur.push_back(K);
         cur.push_back(R);
         cur.push_back(S);
         for(int j = 0; j < K; ++j){
-            scanf("%lld", &temp);
+            scanf("%d", &temp);
             cur.push_back(temp);
         }
         for(int j = 0; j < R; ++j){
-            scanf("%lld", &temp);
+            scanf("%d", &temp);
             cur.push_back(temp);
         }
         rest.push_back(cur);
         cur.clear();
     }
-    search();
-    printf("%lld\n", max_stars);
+
+    for(int i = 1; i <= N; ++i){
+        perm.push_back(i);
+    }
+    do {
+        cur_stars = start_S;
+        flag = true;
+        std::map<int, int> keys_copy = keys;
+        for(int j = 0; j < N; ++j){
+            K = rest[perm[j]][0];
+            R = rest[perm[j]][1];
+            S = rest[perm[j]][2];
+            for(int i = 3; i < 3 + K; ++i){
+                keys_copy[rest[perm[j]][i]]--;
+                if(keys_copy[rest[perm[j]][i]] < 0){
+                    cur_stars = start_S;
+                    max_stars = std::max(max_stars, cur_stars);
+                    flag = false;
+                    break;
+                }
+            }
+            if(!flag) break;
+            for(int i = 3 + K; i < 3 + K + R; ++i){
+                keys_copy[rest[perm[j]][i]]++;
+            }
+            cur_stars += S;
+            max_stars = std::max(max_stars, cur_stars);
+        }
+    }while(std::next_permutation(perm.begin(), perm.end()));
+
+    printf("%d\n", max_stars);
     return 0;
 }
