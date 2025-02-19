544c6acff6103f3049618d4d9964e96e9443d224
Sergio Sakellaropoulos <126912076+sergio-sak@users.noreply.github.com>
2025-02-19 12:02:38 +0200

    added change

--- Pdp 37/B/shroompath/shroompath.cpp
+++ Pdp 37/B/shroompath/shroompath.cpp
@@ -26,7 +26,61 @@ signed main()
     freopen("shroompath.out", "w", stdout);
     
     if(X == 0) printf("%lld\n", (2 + (((ceilY-2)*(expo(2, ceilY))%MOD)%MOD + (expo(2, ceilY - 1) * ceilY)%MOD)%MOD + 1)%MOD);
-    else if(Y == 0  || subtest == 3) printf("%lld\n", (2 + ((ceilX/2 - 1) * expo(2, ceilX/2 + 1))%MOD + ceilX)%MOD);
-    else printf("%lld\n", std::min((2 + ((ceilX/2 - 1) * expo(2, ceilX/2 + 1))%MOD + ceilX)%MOD, (2 + (((ceilY-2)*(expo(2, ceilY))%MOD)%MOD + (expo(2, ceilY - 1) * ceilY)%MOD)%MOD + 1)%MOD));
+    else if(Y == 0  || subtest == 3) printf("%lld\n", ((2 + ((ceilX/2 - 1) * expo(2, ceilX/2 + 1))%MOD + ceilX)%MOD)%MOD);
+    else printf("%lld\n", std::min(((2 + ((ceilX/2 - 1) * expo(2, ceilX/2 + 1))%MOD + ceilX)%MOD)%MOD, (2 + (((ceilY-2)*(expo(2, ceilY))%MOD)%MOD + (expo(2, ceilY - 1) * ceilY)%MOD)%MOD + 1)%MOD));
+    return 0;
+}
+
+
+
+
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#define ll long long
+
+ll subtest, S, X, Y, ceilX, ceilY;
+long long MOD = 1000000007;
+
+ll expo(ll base, ll expon, ll mod = MOD){
+    ll res = 1;
+    while(expon > 0) {
+        if(expon % 2 == 1) res = (res*base) % mod;
+        base = (base*base) % mod;
+        expon /= 2;
+    }
+    return res%mod;
+}
+
+long long computeX(long long S, long long X) {
+    long long ceilSX = (S + X - 1) / X;
+    long long floorHalf = ceilSX / 2;
+    
+    long long powerTerm = expo(2, floorHalf + 1, MOD);
+    long long term = ((floorHalf - 1) * powerTerm ) % MOD;
+    
+    long long result = (2 + term + ceilSX) % MOD;
+    
+    return (result + MOD) % MOD;
+}
+
+long long computeY(long long S, long long Y) {
+    long long ceilSY = (S + Y - 1)/Y;
+    long long powerTerm = expo(2, ceilSY, MOD);
+    long long term1 = ((ceilSY - 2) * powerTerm) % MOD;
+    long long term2 = (powerTerm/2 * ceilSY)%MOD;
+    long long result = 3 + term1 + term2;
+    return (result + MOD) % MOD;
+}
+
+signed main()
+{ 
+    freopen("shroompath.in", "r", stdin);
+    scanf("%lld \n %lld %lld %lld", &subtest, &S, &X, &Y);
+
+    freopen("shroompath.out", "w", stdout);
+    if(X == 0) printf("%lld\n", computeY(S, Y));
+    if(Y == 0) printf("%lld\n", computeX(S, X));
+    else printf("%lld\n", std::min(computeX(S, X), computeY(S, Y)));
     return 0;
 }
